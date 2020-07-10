97fbc3a9dd98be358d8ceec99acf9936b7ed9bb3
Pavel <wol1414@gmail.com>
2020-07-10 21:22:05 +0300

    start tariff

---
+++ sourse/pug/blocks/sRecommended/_sRecommended.js
@@ -0,0 +1,13 @@
+let sRecommendedVue = new Vue({
+		el: '#sRecommended',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
---
+++ sourse/pug/blocks/sSuper/_sSuper.js
@@ -0,0 +1,13 @@
+let sSuperVue = new Vue({
+		el: '#sSuper',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
